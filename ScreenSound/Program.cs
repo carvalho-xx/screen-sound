200bef056ac8a4ba71df5270a757c37880eecca2
João Pedro <jp.carvalhomesqui@gmail.com>
2025-04-10 22:26:17 -0300

    Criando código das opções 1 e 2

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -4,7 +4,97 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> listaDasBandas = new List<string>();
+
+            void ExibirLogo()
+            {
+                Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝
+
+░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+                Console.WriteLine("Seja bem-vindo(a) ao ScreenSound!");
+            }
+
+            void SalvarBanda()
+            {
+                Console.Clear();
+                Console.WriteLine("Salvando uma banda...");
+                Console.Write("Digite o nome da banda que deseja salvar: ");
+                string nomeBanda = Console.ReadLine()!;
+                listaDasBandas.Add(nomeBanda);
+                Console.WriteLine($"A banda {nomeBanda} foi salva com sucesso!");
+                Thread.Sleep(2000);
+                Console.Clear();
+                ExibirOpcoesDeMenu();
+            }
+
+            void ListarBandas()
+            {
+                Console.Clear();
+                Console.WriteLine("Listando todas as bandas");
+                if (listaDasBandas.Count == 0)
+                {
+                    Console.WriteLine("Nenhuma banda cadastrada.");
+                }
+                else
+                {
+                    foreach (string banda in listaDasBandas) 
+                    {
+                        Console.WriteLine($"- {banda}");
+                    }
+                }
+                Console.WriteLine("\nDigite qualquer tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+                ExibirOpcoesDeMenu();
+            }
+
+            void ExibirOpcoesDeMenu()
+            {
+                ExibirLogo();
+                Console.WriteLine(@"
+Digite 1 para salvar uma banda
+Digite 2 para listar todas as bandas
+Digite 3 para avaliar uma banda
+Digite 4 para exibir média de notas de uma banda
+Digite -1 para sair");
+                Console.WriteLine("\nDigite a opção desejada: ");
+                int opcao = int.Parse(Console.ReadLine()!);
+                switch (opcao)
+                {
+                    case 1:
+                        SalvarBanda();
+                        break;
+                    case 2:
+                        ListarBandas();
+                        break;
+                    case 3:
+                        Console.WriteLine("Avaliar uma banda");
+                        break;
+                    case 4:
+                        Console.WriteLine("Exibir média de notas de uma banda");
+                        break;
+                    case -1:
+                        Console.WriteLine("Saindo...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        return;
+                }
+            }
+            
+            ExibirOpcoesDeMenu();
         }
     }
 }
